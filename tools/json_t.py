25b39779e8c56edce5c155f8486bc50417387216
hachimaru <74972395+xxspell@users.noreply.github.com>
2023-09-21 12:58:33 +0500

    Added functions for working with database and JSON

---
+++ tools/json_t.py
@@ -0,0 +1,73 @@
+import json
+import traceback
+import logging
+from settings.reader import admin_id
+
+
+async def update_json_data(chat_id=None, video_id=None, title=None, artist=None):
+    filename = f'current_{admin_id}.json'  # Чтение текущих данных из JSON файла
+    try:
+        with open(filename, 'r') as file:
+
+            data = json.load(file)
+            logging.debug(f'Update Json - {filename}.\n Old data: {data} ')
+    except FileNotFoundError:
+        data = {}
+    except Exception as e:
+        logging.error(f"Ошибка при открытии JSON-файла: {str(e)}")
+        logging.error('Ошибка:\n', traceback.format_exc())
+
+    # Обновление данных, если они переданы
+    if chat_id is not None:
+        data['chat_id'] = chat_id
+    if video_id is not None:
+        data['video_id'] = video_id
+    if title is not None:
+        data['title'] = title
+    if artist is not None:
+        data['artist'] = artist
+    logging.debug(f'Update Json - {filename}.\n New data: {data} ')
+    # Запись обновленных данных обратно в JSON файл
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+
+
+async def edit_json_file(key_to_edit, new_value):
+    filename = f'current_{admin_id}.json'
+    try:
+        # Чтение данных из файла
+        with open(filename, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            logging.debug(f'Update Json - {filename}.\n Old data: {data} ')
+
+        # Изменение данных
+        if key_to_edit in data:
+            data[key_to_edit] = new_value
+            # print(data)
+        else:
+            logging.error(f"Ключ '{key_to_edit}' не найден в JSON-файле.")
+        # print(data)
+        # Запись обновленных данных обратно в файл
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
+        logging.debug(f'Update Json - {filename}.\n New data: {data} ')
+    except Exception as e:
+        logging.error(f"Произошла ошибка при редактировании файла '{filename}': {e}")
+
+
+async def read_json_data():
+    filename = f'current_{admin_id}.json'
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            chat_id = data.get('chat_id')
+            video_id = data.get('video_id')
+            title = data.get('title')
+            artist = data.get('artist')
+            return chat_id, video_id, title, artist
+    except FileNotFoundError:
+        return None
+    except Exception as e:
+        logging.error(f"Ошибка при чтении JSON-файла: {str(e)}")
+        logging.error('Ошибка:\n', traceback.format_exc())
---
+++ tools/sql.py
@@ -0,0 +1,46 @@
+import logging
+import traceback
+
+import pymysql
+from settings.reader import sql_host, sql_user, sql_database, sql_password
+
+from tools.json_t import update_json_data, read_json_data
+from settings.reader import admin_id
+
+connection = pymysql.connect(
+        host=sql_host,
+        user=sql_user,
+        password=sql_password,
+        database=sql_database
+    )
+
+
+async def get_data_db():
+    while True:
+        try:
+            logging.debug('Get data from videos_untrusted')
+            cursor = connection.cursor()
+            # Получаем самую старую запись, у которой checked = 0
+            cursor.execute("SELECT * FROM videos_untrusted WHERE checked = 0 ORDER BY time_listen LIMIT 1")
+            row = cursor.fetchone()
+            if row:
+                video_id, title, artist = row[3], row[1], row[2]
+                chat_id = admin_id  # Замените на ID вашего чата
+                await update_json_data(chat_id, video_id, title, artist)
+
+                # Извлекаем переменные из словаря
+
+                data = await read_json_data()
+                # Проверьте, что данные были успешно получены, прежде чем их распаковать
+                if data is not None:
+                    chat_id, video_id, title, artist = data
+                    # Далее обрабатывайте данные, как вам нужно
+                else:
+                    logging.error("Ошибка при получении данных из JSON.")
+                return chat_id, video_id, title, artist
+
+            else:
+                logging.critical('mda')
+        except Exception as e:
+            logging.error(f"Ошибка при получении данных: {str(e)}")
+            logging.error('Ошибка:\n', traceback.format_exc())
