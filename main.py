ed35f24abfdbf0a70dcfea5ae463729fd91892a4
hachimaru <74972395+xxspell@users.noreply.github.com>
2023-09-22 21:12:37 +0500

    Added registration routers for different_types, track

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from settings.reader import bot_token
+from handlers import different_types, track
 
 
 # Запуск бота
 async def main():
     bot = Bot(token=bot_token, parse_mode="HTML")
     dp = Dispatcher()
+
+    dp.include_routers(different_types.router, track.router)
+    # Альтернативный вариант регистрации роутеров по одному на строку
+    # dp.include_router(questions.router)
+    # dp.include_router(different_types.router)
 
     # Запускаем бота и пропускаем все накопленные входящие
     await bot.delete_webhook(drop_pending_updates=True)
