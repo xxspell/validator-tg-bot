2d03acda1a53fc5106b1105507a024121306b74d
hachimaru <74972395+xxspell@users.noreply.github.com>
2023-09-24 11:14:15 +0500

    Added a record of the current message id from the bot

---
+++ tools/message.py
@@ -0,0 +1,11 @@
+import traceback
+import logging
+
+
+async def save_message_id(message_id):
+    try:
+        with open('bot_data.txt', 'w') as file:
+            file.write(message_id)
+    except Exception as e:
+        logging.error(f"Ошибка при создания айди сообщения: {str(e)}")
+        logging.error('Ошибка:\n', traceback.format_exc())
