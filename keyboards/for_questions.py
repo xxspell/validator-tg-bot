bdd815f1857971888bc80e63934704542ec99680
hachimaru <74972395+xxspell@users.noreply.github.com>
2023-09-26 22:25:03 +0500

    Add inline keyboard for message track

--- keyboards/for_questions.py
+++ keyboards/for_questions.py
@@ -1,4 +1,6 @@
 from aiogram import types
+from aiogram.types import InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 def default_keyboard():
@@ -10,3 +12,13 @@ def default_keyboard():
                                          input_field_placeholder="♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥")
 
     return keyboard
+
+
+def manage_track() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(text="Верно", callback_data="approve"), )
+    builder.add(types.InlineKeyboardButton(text="Изменить название", callback_data="edit_name"), )
+    builder.add(types.InlineKeyboardButton(text="Изменить исполнителя", callback_data="edit_artist"), )
+    builder.add(types.InlineKeyboardButton(text="Удалить", callback_data="delete"), )
+    builder.adjust(2)
+    return builder.as_markup(resize_keyboard=True)
