de163732c3c6b73c9e04e75f7a258740baa1786b
hachimaru <74972395+xxspell@users.noreply.github.com>
2023-09-29 18:25:49 +0500

    Add admin check

--- handlers/different_types.py
+++ handlers/different_types.py
@@ -1,11 +1,18 @@
 from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command, CommandStart
+from handlers.filters import Admin
 from keyboards.for_questions import default_keyboard
 router = Router()
 
 
-@router.message(CommandStart())
-@router.message(F.text.casefold() == "start")
+@router.message(~Admin())
+async def not_admin(message: Message):
+    await message.answer_sticker(r'CAACAgIAAxkBAAIpR2UdZS6SZI_iSRh3j4w7Fn0vn8abAALTAAM8ilcakZtNoizuo7owBA')
+    await message.answer("@xxspell")
+
+
+@router.message(Admin(), CommandStart())
+@router.message(Admin(), F.text.casefold() == "start")
 async def start(message: Message):
     await message.answer("Доступ разрешён, выберете действие(статистика не сделана):", reply_markup=default_keyboard())
---
+++ handlers/filters.py
@@ -0,0 +1,8 @@
+from aiogram.filters import Filter
+from aiogram.types import Message
+from settings.reader import admin_id
+
+
+class Admin(Filter):
+    async def __call__(self, message: Message) -> bool:
+        return message.from_user.id == int(admin_id)
